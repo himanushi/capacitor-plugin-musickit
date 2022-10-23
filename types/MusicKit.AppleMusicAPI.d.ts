39dacf6e9cf9cd11a826d5c8eede37925d4ecb1c
himanushi <himanushi@users.noreply.github.com>
2022-10-24 07:34:19 +0900

    add api urls

--- types/MusicKit.AppleMusicAPI.d.ts
+++ types/MusicKit.AppleMusicAPI.d.ts
@@ -4,33 +4,68 @@ declare namespace MusicKit {
     type ID = `/${string}` | "";
 
     type AlbumsUrl =
+      | `/v1/me/library/albums/${string}/catalog`
       | `/v1/catalog/${string}/albums${ID}`
-      | `/v1/me/library/albums/${string}/catalog`;
+      | `/v1/catalog/${string}/artists/${string}/albums`
+      | `/v1/catalog/${string}/songs/${string}/albums`
+      | `/v1/catalog/${string}/music-videos/${string}/albums`;
     type ArtistsUrl =
+      | `/v1/me/library/artists/${string}/catalog`
       | `/v1/catalog/${string}/artists${ID}`
-      | `/v1/catalog/${string}/albums/${string}/artists`;
+      | `/v1/catalog/${string}/albums/${string}/artists`
+      | `/v1/catalog/${string}/songs/${string}/artists`
+      | `/v1/catalog/${string}/songs/${string}/composers`
+      | `/v1/catalog/${string}/music-videos/${string}/artists`;
     type SongsUrl =
+      | `/v1/me/library/songs/${string}/catalog`
       | `/v1/catalog/${string}/songs${ID}`
-      | `/v1/catalog/${string}/albums/${string}/tracks`;
-    type MusicVideosUrl = `/v1/catalog/${string}/music-videos${ID}`;
-    type PlaylistsUrl = `/v1/catalog/${string}/playlists${ID}`;
-    type StationsUrl = `/v1/catalog/${string}/stations${ID}`;
-    type GenresUrl = `/v1/catalog/${string}/albums/${string}/genres`;
+      | `/v1/catalog/${string}/albums/${string}/tracks`
+      | `/v1/catalog/${string}/music-videos/${string}/songs`
+      | `/v1/catalog/${string}/playlists/${string}/tracks`;
+    type MusicVideosUrl =
+      | `/v1/me/library/music-videos/${string}/catalog`
+      | `/v1/catalog/${string}/music-videos${ID}`
+      | `/v1/catalog/${string}/artists/${string}/music-videos`
+      | `/v1/catalog/${string}/songs/${string}/music-videos`;
+    type PlaylistsUrl =
+      | `/v1/me/library/playlists/${string}/catalog`
+      | `/v1/catalog/${string}/playlists${ID}`
+      | `/v1/catalog/${string}/artists/${string}/playlists`;
+    //  `/v1/catalog/${string}/playlists/${string}/curator`
+    type StationsUrl =
+      | `/v1/catalog/${string}/stations${ID}`
+      | `/v1/catalog/${string}/artists/${string}/station`
+      | `/v1/catalog/${string}/songs/${string}/station`;
+    type GenresUrl =
+      | `/v1/catalog/${string}/albums/${string}/genres`
+      | `/v1/catalog/${string}/artists/${string}/genres`
+      | `/v1/catalog/${string}/songs/${string}/genres`
+      | `/v1/catalog/${string}/music-videos/${string}/genres`;
     type RecordLabelsUrl =
       `/v1/catalog/${string}/albums/${string}/record-labels`;
 
     type LibraryAlbumsUrl =
+      | `/v1/catalog/${string}/albums/${string}/library`
       | `/v1/me/library/albums${ID}`
       | `/v1/me/library/artists/${string}/albums`
       | `/v1/me/library/songs/${string}/albums`
-      | `/v1/me/library/music-videos/${string}/albums`
-      | `/v1/catalog/${string}/albums/${string}/library`;
+      | `/v1/me/library/music-videos/${string}/albums`;
     type LibraryArtistsUrl =
       | `/v1/me/library/artists${ID}`
-      | `/v1/me/library/albums/${string}/artists`;
-    type LibrarySongsUrl = `/v1/me/library/songs${ID}`;
-    type LibraryMusicVideosUrl = `/v1/me/library/music-videos${ID}`;
-    type LibraryPlaylistsUrl = `/v1/me/library/playlists${ID}`;
+      | `/v1/me/library/albums/${string}/artists`
+      | `/v1/me/library/songs/${string}/artists`
+      | `/v1/me/library/music-videos/${string}/artists`;
+    type LibrarySongsUrl =
+      | `/v1/catalog/${string}/songs/${string}/library`
+      | `/v1/me/library/songs${ID}`
+      | `/v1/me/library/albums/${string}/tracks`
+      | `/v1/me/library/playlists/${string}/tracks`;
+    type LibraryMusicVideosUrl =
+      | `/v1/catalog/${string}/music-videos/${string}/library`
+      | `/v1/me/library/music-videos${ID}`;
+    type LibraryPlaylistsUrl =
+      | `/v1/catalog/${string}/playlists/${string}/library`
+      | `/v1/me/library/playlists${ID}`;
 
     type RatingCategoryType = "artists" | "albums" | "songs" | "playlists";
     type RatingType = RatingCategoryType | `library-${RatingCategoryType}`;
