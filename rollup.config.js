6edd6611ef5c5dfbeef146b17df3dcf0bd76ed51
himanushi <himanushi@users.noreply.github.com>
2022-10-25 20:10:12 +0900

    ビルド修正

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,17 @@ export default {
     {
       file: "dist/plugin.js",
       format: "iife",
+      globals: {
+        "@capacitor/core": "capacitorExports",
+      },
       inlineDynamicImports: true,
       name: "capacitorCapacitorMusicKit",
+      sourcemap: true,
+    },
+    {
+      file: "dist/plugin.cjs.js",
+      format: "cjs",
+      inlineDynamicImports: true,
       sourcemap: true,
     },
   ],
